220d33ba688fc31f17f093e15479555bd558da6e
ReginaChege <reginachegewairimu@gmail.com>
2023-05-06 00:37:18 +0300

    first

--- pytho.py
+++ pytho.py
@@ -32,7 +32,7 @@ print(common_number())
 # that are divisible by any single digit besides 1 (2-9)
 def divisible_by_2_and_9():
     divisible= [x for x in range(1, 1001) if any(x % number == 0 for number in range(2, 10))]
-print(divisible)
+print(divisible_by_2_and_9())
 
 #  Given a list of numbers, use list comprehension to remove all odd numbers from the list:
 # numbers = [3,5,45,97,32,22,10,19,39,43]
@@ -43,9 +43,31 @@ def remove_odd_numbers(num):
             return x
         else:
             return "none"
+    count
 num = [3,5,45,97,32,22,10,19,39,43]
 print(remove_odd_numbers(num))
 
 # 10. Write a Python function to remove all vowels in a string
-def remove_vowels():
-    
+def remove_vowels(word):
+    empty=[]
+    vowels="aeiouAEIOU"
+    for x in word:
+        if x in vowels:
+            if x==vowels:
+                return empty.push(x)
+print(remove_vowels("Regina"))
+
+
+# 5. Write a Python program to convert a list to a list of dictionaries.
+# Sample lists: ["Black", "Red", "Maroon", "Yellow"], ["#000000", "#FF0000", "#800000", "#FFFF00"]
+
+
+
+# 10. Write a Python function to remove all vowels in a string
+
+
+# 6. Write a Python program to check whether all dictionaries in a list are empty or not
+
+
+# 3. Write a Python program that accepts a comma-separated sequence of words as input
+# and prints the distinct words in sorted form (alphanumerically).
